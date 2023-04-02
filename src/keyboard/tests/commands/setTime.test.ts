a3df4e253482cd4361da4385c3c99b0220df7df5
Kishor Prins <me@68x.ca>
2023-04-02 09:47:32 -0400

    Attempt to fix the generateSetTime spec to use a set mock date with seconds

--- src/keyboard/tests/commands/setTime.test.ts
+++ src/keyboard/tests/commands/setTime.test.ts
@@ -4,7 +4,7 @@ import { generateSetTime } from '../../commands/setTime';
 
 describe('generateSetTime', ()=>{
   test('generates a buffer containing the data to be sent to the keyboard when it is daylight savings time', () => {
-    const testDate = new Date('March 23, 2023 09:00 EDT');
+    const testDate = new Date('March 23, 2023 09:00:00 EDT');
     const expectedData = Buffer.from('0103641c5b6001050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007', 'hex');
 
     jest.useFakeTimers().setSystemTime(testDate);
@@ -15,7 +15,7 @@ describe('generateSetTime', ()=>{
   });
 
   test('generates a buffer containing the data to be sent to the keyboard when it is not daylight savings time', () => {
-    const testDate = new Date('Dec 23, 2023 09:00 EDT');
+    const testDate = new Date('Dec 23, 2023 09:00:00 EDT');
     const expectedData = Buffer.from('01036586d9d00105000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002a', 'hex');
 
     jest.useFakeTimers().setSystemTime(testDate);
