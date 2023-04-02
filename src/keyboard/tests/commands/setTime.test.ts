1b5a5b21ce6b5b2705680ca82866cbb95dec56f7
Kishor Prins <me@68x.ca>
2023-04-02 11:46:59 -0400

    Fix Jest faketimers not working across timezones

--- src/keyboard/tests/commands/setTime.test.ts
+++ src/keyboard/tests/commands/setTime.test.ts
@@ -4,23 +4,23 @@ import { generateSetTime } from '../../commands/setTime';
 
 describe('generateSetTime', ()=>{
   test('generates a buffer containing the data to be sent to the keyboard when it is daylight savings time', () => {
-    const testDate = new Date('March 23, 2023 09:00:00 EDT');
-    const expectedData = Buffer.from('0103641c5b6001050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007', 'hex');
+    const testDate: Date = new Date('March 23, 2022 09:00:00 PDT');
+    const expectedData = Buffer.from('0103623b440001040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000089', 'hex');
 
-    jest.useFakeTimers().setSystemTime(testDate);
+    const spy = jest.spyOn(global, 'Date').mockImplementation(() => testDate);
     const generatedData = generateSetTime();
-    jest.useRealTimers();
+    spy.mockRestore();
 
     expect(generatedData).toEqual(expectedData);
   });
 
   test('generates a buffer containing the data to be sent to the keyboard when it is not daylight savings time', () => {
-    const testDate = new Date('Dec 23, 2023 09:00:00 EDT');
-    const expectedData = Buffer.from('01036586d9d00105000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002a', 'hex');
+    const testDate = new Date('Dec 23, 2022 09:00:00 PDT');
+    const expectedData = Buffer.from('010363a5d0800105000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008d', 'hex');
 
-    jest.useFakeTimers().setSystemTime(testDate);
+    const spy = jest.spyOn(global, 'Date').mockImplementation(() => testDate);
     const generatedData = generateSetTime();
-    jest.useRealTimers();
+    spy.mockRestore();
 
     expect(generatedData).toEqual(expectedData);
   });
