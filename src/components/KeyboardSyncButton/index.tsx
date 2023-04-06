2cdc983c9e8f71f214d89d89814efbfffcfc7040
Kishor Prins <me@68x.ca>
2023-04-06 18:13:43 -0400

    Add tests for the sync button

--- src/components/KeyboardSyncButton/index.tsx
+++ src/components/KeyboardSyncButton/index.tsx
@@ -8,10 +8,10 @@ export default () => {
   const selectedPort = useAppSelector(state => state.keyboardPorts.selectedPort);
 
   const sync = async () => {
-    await window.keyboardAPI.setTime(selectedPort.path);
+    await window.keyboardAPI.setTime(selectedPort?.path);
   };
 
   return (
-    <button className="rounded-sm bg-white p-0.25 px-1 hover:bg-gray-200" onClick={sync}>Sync</button>
+    <button className="rounded-sm bg-white p-0.25 px-1 hover:bg-gray-200" data-testid="sync-button" onClick={sync}>Sync</button>
   );
 };
