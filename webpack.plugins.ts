b1329c84c0b06bdaafd680d87d0ff111731de186
Kishor Prins <me@68x.ca>
2023-05-12 09:05:04 -0700

    Disable BundleAnalyzer from opening a browser window

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -4,16 +4,18 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const BundleAnalyzerPlugin: typeof IForkTsCheckerWebpackPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const CompressionPlugin: typeof IForkTsCheckerWebpackPlugin = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
-  new BundleAnalyzerPlugin(),
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+  }),
   new CompressionPlugin(),
 ];
