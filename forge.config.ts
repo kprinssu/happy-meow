6b97ce3205b681ef6ef155f6168042ca50b5774b
Kishor Prins <me@68x.ca>
2023-05-15 10:57:42 -0700

    Fix up packaging errors with serialport

--- forge.config.ts
+++ forge.config.ts
@@ -5,12 +5,16 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { spawn } from 'child_process';
+
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
-    icon: 'assets/icon.png',
+    icon: './assets/icon.png',
   },
   rebuildConfig: {},
   makers: [
@@ -27,8 +31,16 @@ const config: ForgeConfig = {
           genericName: 'Happy Meow',
           description: 'Keyboard utility for Cyberboard owners',
           categories: ['Utility'],
-        }
-      }
+        },
+        modules: [{
+          name: 'zypak',
+          sources: [{
+            type: 'git',
+            url: 'https://github.com/refi64/zypak',
+            tag: 'v2022.04',
+          }],
+        }],
+      },
     },
   ],
   plugins: [
@@ -51,16 +63,48 @@ const config: ForgeConfig = {
     {
       name: '@electron-forge/plugin-electronegativity',
       config: {
-        isSarif: true
-      }
-    },
-    {
-      name: '@electron-forge/plugin-electronegativity',
-      config: {
-        isSarif: true
-      }
+        isSarif: true,
+      },
     },
   ],
+  hooks: {
+    readPackageJson: async (forgeConfig, packageJson) => {
+      // only copy deps if there isn't any
+      if (Object.keys(packageJson.dependencies).length === 0) {
+        const originalPackageJson = await fs.readJson(path.resolve(__dirname, 'package.json'));
+        const webpackConfigJs = require('./webpack.renderer.config.js');
+
+        Object.keys(webpackConfigJs.externals).forEach(jsPkg => {
+          packageJson.dependencies[jsPkg] = originalPackageJson.dependencies[jsPkg];
+        });
+      }
+
+      return packageJson;
+    },
+    packageAfterPrune: async (_forgeConfig, buildPath) => {
+      console.log(buildPath);
+      return new Promise((resolve, reject) => {
+        const yarnInstall = spawn('yarn', ['install'], {
+          cwd: buildPath,
+          stdio: 'inherit',
+          shell: true
+        });
+
+        yarnInstall.on('close', (code) => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject(new Error('process finished with error code ' + code));
+          }
+        });
+
+        yarnInstall.on('error', (error: unknown) => {
+          reject(error);
+        });
+      });
+    }
+  },
 };
 
 export default config;
+
